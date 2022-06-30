fdb06c834f7d9978e07538efeea87e38e6933917
a-olusola <a.olusola-charles@gmail.com>
2022-06-30 14:27:02 -0700

    C code to print an output

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
 int main(void)
-
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+*     */
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
