423c393986356d2ff284440f95d44a86b479c515
a-olusola <a.olusola-charles@gmail.com>
2022-07-06 03:02:02 -0700

    Function that checks for lower case characters

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,6 @@
  *
  * * lowercase character. Another cases, shows
  *   0
- *  
  * * @c: The character in ASCII code
  *
  * * Return: 1 for lowercase character. 0 for the rest.
@@ -21,7 +20,6 @@ int _islower(int c)
 	{
 		return (0);
 	}
-	
 	_putchar('\n');
 
 }
