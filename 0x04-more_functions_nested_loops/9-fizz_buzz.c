02133ba4f76e5fa926dfcc55ba828c45c2cd1410
a-olusola <a.olusola-charles@gmail.com>
2022-07-07 15:53:04 -0700

    fizzbuzz added

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 	int n;
 
 	n = 1;
-	printf("%\d", n);
+	printf("%d", n);
 	for (n = 2; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
@@ -28,7 +28,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%\d", n);
+			printf(" %d", n);
 		}
 	}
 	printf("\n");
