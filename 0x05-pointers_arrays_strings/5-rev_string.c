0a3f8e58454f6755e8c5fda6b5b976c23bcf1c2b
a-olusola <a.olusola-charles@gmail.com>
2022-07-13 03:10:24 -0700

    betty Style corrected

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -3,13 +3,13 @@
 /**
  * * rev_string - reverses a string.
  * * @s: input string.
- * * Return: no return.
+ * * Return: no return
  */
 void rev_string(char *s)
 {
 	int count = 0, i, j;
 	char *str, temp;
-	
+
 	while (count >= 0)
 	{
 		if (s[count] == '\0')
