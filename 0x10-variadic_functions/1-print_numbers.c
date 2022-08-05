513315206994ae2871724f6629cc0c0355b45f2d
a-olusola <a.olusola-charles@gmail.com>
2022-08-05 01:34:24 -0700

    Betty style corrected

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,6 +11,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
+
 	va_start(valist, n);
 
 	for (i = 0; i < n; i++)
@@ -21,5 +22,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			printf("%s", separator);
 	}
 	printf("\n");
+
 	va_end(valist);
 }
